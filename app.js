b18d634691270317628b79ce538ac8942f679494
khalilpyh <khalilpyh@gmail.com>
2023-07-06 03:24:28 -0400

    Intergrate MongoDB into project for storing and managing data.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,8 +11,37 @@ app.use(express.static("public"));
 //set up EJS
 app.set("view engine", "ejs");
 
-//declare a list to hold new items
-let items = ["Buy Food", "Cook Food", "Eat Food"]; //fill with sample items
+//connect to MongoDB and create a database to hold todo items
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+//create database schema
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+//create database model
+const Item = mongoose.model("Item", itemsSchema);
+
+//supply some sample items as default data
+const item1 = new Item({
+  name: "Buy Food",
+});
+const item2 = new Item({
+  name: "Cook Food",
+});
+const item3 = new Item({
+  name: "Eat Food",
+});
+
+const defaultItems = [item1, item2, item3];
+
+// //declare a list to hold new items
+// let items = ["Buy Food", "Cook Food", "Eat Food"]; //fill with sample items
 let workItems = [];
 
 //Landing Page - Get
@@ -19,20 +49,48 @@ app.get("/", function (req, res) {
   //get the current date
   let day = date.getDate();
 
-  res.render("list", { listTitle: day, newListItems: items });
+  //display all the items retrieved from the database
+  Item.find()
+    .then((foundItems) => {
+      //if database is empty, insert some default items into database
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems)
+          .then(() => {
+            console.log("Successfully added the default items.");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: day, newListItems: foundItems });
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 //Landing Page - Post
 app.post("/", function (req, res) {
-  //retrieve user input item
-  let newItem = req.body.newItem;
+  //retrieve user input item name
+  let itemName = req.body.newItem;
 
-  //add item base on differenct page button post
+  //add item tp the database base on differenct page button post
   if (req.body.list === "Work") {
     workItems.push(newItem);
     res.redirect("/work");
   } else {
-    items.push(newItem);
+    //create a new item object and save to database
+    const newItem = new Item({
+      name: itemName,
+    });
+
+    newItem.save()
+      .then(() => {
+        console.log("Successfully added the new item.");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     res.redirect("/");
   }
 });
@@ -55,7 +113,6 @@ app.post("/work", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("about");
 });
-
 
 //Set up Port
 app.listen(3000, function () {
